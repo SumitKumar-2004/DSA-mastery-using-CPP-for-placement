196141020de2130a483f297ced6f3be7afed3ad3
Sumit <skdudy546@gmail.com>
2026-06-04 16:15:44 +0530

    Leetcode 344

---
+++ Strings/Reverse.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void reverseString(vector<char> &s)
+{
+   int left = 0;
+   int right = s.size() - 1;
+   while (left < right)
+   {
+      swap(s[left], s[right]);
+      left++;
+      right--;
+   }
+}
+int main()
+{
+   vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+   reverseString(s);
+   for (auto it : s)
+   {
+      cout << it;
+   }
+   return 0;
+}
