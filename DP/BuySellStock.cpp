059e7654b55a8db217084698082a7ceed84e494a
Sumit <skdudy546@gmail.com>
2026-06-17 16:41:22 +0530

    Leetcode 121

--- DP/BuySellStock.cpp
+++ DP/BuySellStock.cpp
@@ -1,16 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main()
+{
+   // Stock prices on different days
    vector<int> prices = {7,1,5,6,3,4};
+
+   // Total number of days
    int n = prices.size();
+
+   // Stores maximum profit found so far
    int profit = 0;
+
+   // Stores minimum stock price seen so far
+   // Initially first day's price
    int mini = prices[0];
+
+   // Traverse all days
    for (int i = 0; i < n; i++)
    {
+      // Profit if we buy at minimum price
+      // and sell on current day
       int cost = prices[i] - mini;
-      profit = max(profit,cost);
-      mini = min(mini,prices[i]);
+
+      // Update maximum profit if current profit is larger
+      profit = max(profit, cost);
+
+      // Update minimum price seen so far
+      mini = min(mini, prices[i]);
    }
 
-   cout<<profit;
+   // Print maximum profit
+   cout << profit;
 }
