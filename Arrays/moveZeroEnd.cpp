db464dc48c722d721d193c32f16a683bd4c9217a
Sumit <skdudy546@gmail.com>
2026-06-17 08:05:22 +0530

    Leetcode 283

--- Arrays/moveZeroEnd.cpp
+++ Arrays/moveZeroEnd.cpp
@@ -1,32 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-void moveZero(vector<int> &arr, int n)
+
+// Function to move all zeroes to the end
+void moveZero(vector<int> &arr)
 {
-   int j = -1;
-   for (int i = 0; i < n; i++)
+   // j points to the position where
+   // the next non-zero element should be placed
+   int j = 0;
+
+   // Traverse the entire array
+   for (int i = 0; i < arr.size(); i++)
    {
-      if (arr[i] == 0)
-      {
-         j = i;
-         break;
-      }
-   }
-   for (int i = j + 1; i < n; i++)
-   {
+      // If current element is non-zero
       if (arr[i] != 0)
       {
+         // Place the non-zero element at position j
          swap(arr[i], arr[j]);
+
+         // Move j to the next position
          j++;
       }
    }
 }
+
 int main()
 {
+   // Input array containing zeroes
    vector<int> arr = {1, 0, 4, 3, 5, 0, 3, 0, 0, 2, 7};
-   int n = arr.size();
-   moveZero(arr, n);
+
+   // Move all zeroes to the end
+   moveZero(arr);
+
+   // Print modified array
    for (int x : arr)
    {
       cout << x << " ";
    }
+
+   return 0;
 }
