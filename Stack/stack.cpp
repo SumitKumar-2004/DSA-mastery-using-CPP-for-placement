3e0fa540e289ec84aaceb95ac85276aa587dbb76
Sumit <skdudy546@gmail.com>
2026-06-18 16:30:13 +0530

    Stack creation

---
+++ Stack/stack.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   stack<int> st;
+   st.push(10);
+   st.push(20);
+   st.push(30);
+   st.push(40);
+   cout << st.top() << endl;
+   st.pop();
+   cout << st.top() << endl;
+   cout << st.size() << endl;
+   return 0;
+}
