42743f68b310bea79f2ff78df61717d43ac1d3d5
Sumit <skdudy546@gmail.com>
2026-06-23 16:07:07 +0530

    Leetcode 58

---
+++ Strings/lengthOfLastWord.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string str = "   fly me   to   the moon  ";
+   int i = str.size() - 1;
+
+   //we check from the end of the string
+
+   //check for spaces
+   while(i >=0 && str[i] == ' '){
+      i--;
+}
+   int count = 0;
+   //if no white space we count the length
+   while(i >= 0 && str[i] != ' '){
+      count++;
+      i--;
+   }
+   cout<<"The length of last word is :"<<count;
+}
