45dc4ca53036c98f9a873da7f4e98880e78e1d11
Sumit <skdudy546@gmail.com>
2026-06-26 17:40:06 +0530

    Binary to Octal Conversion

--- Bit Manipulation/binaryToOctal.cpp
+++ Bit Manipulation/binaryToOctal.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   string binary = "1111";
+   string binary = "1011";
    int octal = 0;
    for(int i = 0; i < binary.size(); i++){
         int bit = binary[i] - '0';
